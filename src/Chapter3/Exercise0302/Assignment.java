c44eb58cfa529d9fe998c6a5cd29048dd86fc637
XiaoMing <l>
2020-06-29 12:38:48 +0800

    first commit

---
+++ src/Chapter3/Exercise0302/Assignment.java
@@ -0,0 +1,20 @@
+package Chapter3.Exercise0302;
+
+/**
+ * @author:YiMing
+ * @create:2020/6/29,12:32
+ * @version:1.0
+ */
+public class Assignment {
+    public static void main(String[] args) {
+        Tank t1 = new Tank();
+        Tank t2 = new Tank();
+
+        t1.level = 9;
+        t2.level = 47;
+
+        System.out.println("t1.level="+t1.level+" t2.level="+t2.level);
+
+
+    }
+}
---
+++ src/Chapter3/Exercise0302/Tank.java
@@ -0,0 +1,10 @@
+package Chapter3.Exercise0302;
+
+/**
+ * @author:YiMing
+ * @create:2020/6/29,12:31
+ * @version:1.0
+ */
+public class Tank {
+    int level;
+}
