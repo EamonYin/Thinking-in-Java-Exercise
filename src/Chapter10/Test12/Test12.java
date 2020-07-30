b2cc19134ba39a86e772a96697966a425b91c8ba
XiaoMing <l>
2020-07-30 22:28:35 +0800

    Chapter10

--- src/Chapter10/Test12/Test12.java
+++ src/Chapter10/Test12/Test12.java
@@ -5,7 +5,7 @@ package Chapter10.Test12;
  * @create:2020/7/30,18:42
  * @version:1.0
  */
-public class Test7 {
+public class Test12 {
     public static void main(String[] args) {
         Outer outer = new Outer();
         outer.h();
