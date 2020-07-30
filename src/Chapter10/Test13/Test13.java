d747605719289a1805e947a20f0dba5ea690eaab
XiaoMing <l>
2020-07-30 22:33:20 +0800

    Chapter10

---
+++ src/Chapter10/Test13/Test13.java
@@ -0,0 +1,13 @@
+package Chapter10.Test13;
+
+/**
+ * @author:YiMing
+ * @create:2020/7/30,21:11
+ * @version:1.0
+ */
+public class Test13 {
+    public static void main(String[] args) {
+        ImplOuter implOuter = new ImplOuter();
+        implOuter.getOuter().IsOuter();
+    }
+}
