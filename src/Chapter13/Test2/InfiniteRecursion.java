b91c89ce713d02fa2a42fa2fc123d0cc6dfe2b21
XiaoMing <l>
2020-08-15 11:25:35 +0800

    Chapter13

--- src/Chapter13/Test2/InfiniteRecursion.java
+++ src/Chapter13/Test2/InfiniteRecursion.java
@@ -7,7 +7,7 @@ package Chapter13.Test2;
  */
 public class InfiniteRecursion {
     public String toString() {
-        //用super.toString()不用this；因为this会被类型转换，从而发生递归调用
+        //用super.toString()不用this;因为this会被类型转换，从而发生递归调用
         return "This is " + super.toString() + "\n";
     }
 }
