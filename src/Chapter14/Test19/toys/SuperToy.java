85bf6c4fc90212affc108d3b4b27fd1d409b51f1
XiaoMing <l>
2020-08-20 23:02:30 +0800

    Chapter14

--- src/Chapter14/Test19/toys/SuperToy.java
+++ src/Chapter14/Test19/toys/SuperToy.java
@@ -22,6 +22,6 @@ public class SuperToy extends FancyToy {
     }
 
     public String toString() {
-        return "I'm a SuperToy. I'm smarter than you.";
+        return "【SuperToy】创建成功";
     }
 }
