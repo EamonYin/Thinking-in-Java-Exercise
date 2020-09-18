f406dd12d5b100d33fa9d44f30ad185ab743b6fb
进阶的小名 <l>
2020-09-18 16:21:09 +0800

    Chapter20

--- src/Chapter20/atunit/Test.java
+++
@@ -1,8 +0,0 @@
-//: net/mindview/atunit/Test.java
-// The @Test tag.
-package Chapter20.atunit;
-import java.lang.annotation.*;
-
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-public @interface Test {} ///:~
